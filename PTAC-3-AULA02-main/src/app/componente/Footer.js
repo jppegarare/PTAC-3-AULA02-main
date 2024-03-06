4ecf414dfc6fb8790963ed01fd427562074115b5
João Pegarare <jppegarare18@gmail.com>
2024-03-06 09:30:47 -0400

    estilização e Map
    Fiz uma pequena estilização no footer e not-found e fiz o componente Map, coloquei ele no layout

--- PTAC-3-AULA02-main/src/app/componente/Footer.js
+++ PTAC-3-AULA02-main/src/app/componente/Footer.js
@@ -3,12 +3,7 @@ import styles from "./footer.module.css"
 export default function Footer(){
     return(
         <header header className={styles.cabecalho}>
-            <Image width={100} height={100} src={"https://ead.ifms.edu.br/theme/moove/pix/default_course.jpg"}/>
-            <nav>
-
-                <p>copyright</p>
-
-            </nav>
+            <Image width={200} height={150} src={"https://ead.ifms.edu.br/theme/moove/pix/default_course.jpg"}/>
         </header>
     )
 }
--- PTAC-3-AULA02-main/src/app/not-found.js
+++ PTAC-3-AULA02-main/src/app/not-found.js
@@ -1,6 +1,6 @@
 import styles from "./not-found.module.css"
 export default function NotFound(){
     return(
-        <h1 className={styles.texto}>A pagina não existe man!</h1>
+        <h1 className={styles.texto}>A pagina não existe!</h1>
     )
 }
